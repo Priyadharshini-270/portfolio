7b26513ff10e76125e9816a6906effe31a1f14b9
devi priyadharshni <priyqpriya986@gmail.com>
2026-09-22 10:33:10 +0530

    Updated portfolio

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-var words = ["Full Stack Developer", "Web Designer"];
+var words = ["Full Stack Developer", "Data Analyst", "Data Analytics"];
 var i = 0;
 var j = 0;
 var currentWord = "";
 var isDeleting = false;
 
-function type(){
+function type() {
   currentWord = words[i];
 
-  if(!isDeleting){
-    document.querySelector(".typing").textContent =
-      currentWord.substring(0, j+1);
-    j++;
-    if(j === currentWord.length){
-      isDeleting = true;
-      setTimeout(type, 1000);
-      return;
-    }
-  } else {
-    document.querySelector(".typing").textContent =
-      currentWord.substring(0, j-1);
+  if (isDeleting) {
     j--;
-    if(j === 0){
-      isDeleting = false;
-      i = (i + 1) % words.length;
-    }
+  } else {
+    j++;
   }
-  setTimeout(type, isDeleting ? 60 : 120);
+
+  document.querySelector(".typing").textContent = currentWord.substring(0, j);
+
+  if (!isDeleting && j === currentWord.length) {
+    isDeleting = true;
+    setTimeout(type, 1000); // pause
+    return;
+  }
+
+  if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % words.length;
+  }
+
+  setTimeout(type, isDeleting ? 50 : 100);
 }
 
 type();
